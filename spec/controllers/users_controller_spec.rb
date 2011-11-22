237dd432f2a1ef5985fe95a47dc5f7ee730c4a27
Matt <shadowed666shadowed@yahoo.com>
2011-11-22 10:08:23 -0600

    user sign up

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,5 +14,37 @@ require 'spec_helper'
       response.should have_selector("title", :content => "Sign up")
     end
   end
+  
+   describe "POST 'create'" do
+    describe "success" do
+      
+      before(:each) do
+        @attr = {:name => "John Doe", :email => "jdoe@example.com"}
+      end
+      
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+    end
+  end
+  
+    def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 
 end
